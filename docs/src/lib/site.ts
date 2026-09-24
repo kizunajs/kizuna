c6e341f4d00cfbfd653be6efe7819014208fd519
Sondre Ørland <78776152+sondreorland@users.noreply.github.com>
2026-09-24 22:52:33 +0200

    ci: sync the docs site version on release (#195)

--- docs/src/lib/site.ts
+++ docs/src/lib/site.ts
@@ -2,6 +2,6 @@ export const siteUrl = 'https://kizunajs.com';
 
 export const githubUrl = 'https://github.com/kizunajs/kizuna';
 
-export const version = '2.0.0-beta.5';
+export const version = '2.0.0-beta.7';
 
 export const npmUrl = `https://www.npmjs.com/package/kizunajs/v/${version}`;
